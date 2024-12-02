80d33b88a061866ecd0ac81e5deff38be1d79116
Denis S Dujota <denisdujota@gmail.com>
2024-12-02 14:48:49 +0300

    finish many to many

--- main_app/views.py
+++ main_app/views.py
@@ -17,10 +17,13 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
+    # toys = Toy.objects.all()
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(request, 'cats/detail.html', {
         'cat': cat,
-        'feeding_form': feeding_form
+        'feeding_form': feeding_form,
+        'toys': toys_cat_doesnt_have
         })
 
 def add_feeding(request, cat_id):
@@ -72,3 +75,18 @@ class ToyUpdate(UpdateView):
 class ToyDelete(DeleteView):
     model = Toy
     success_url = '/toys/'
+
+
+    # Note that you can pass a toy's id instead of the whole object
+def associate_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
+
+def remove_toy(request, cat_id, toy_id):
+    # Look up the cat
+    cat = Cat.objects.get(id=cat_id)
+    # Look up the toy
+    toy = Toy.objects.get(id=toy_id)
+    # Remove the toy from the cat
+    cat.toys.remove(toy)
+    return redirect('cat-detail', cat_id=cat.id)
