f2643090c6db4fe6e6935eb68e1bc6082a692f97
Denis S Dujota <denisdujota@gmail.com>
2024-12-02 16:20:41 +0300

    finish auth lesson

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
+    path('accounts/signup/', views.signup, name='signup'),
     # Cats
     path('cats/', views.cat_index, name='cat-index'),
     path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
--- main_app/views.py
+++ main_app/views.py
@@ -2,19 +2,50 @@ from django.shortcuts import render, redirect
 from .models import Cat, Toy
 from .forms import FeedingForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
 from django.views.generic import DetailView, ListView
 
-def home(request):
-    return render(request, 'home.html')
+# AUTH
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# def home(request):
+#     return render(request, 'home.html')
+
+class Home(LoginView):
+    template_name = 'home.html'
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in
+            login(request, user)
+            return redirect('cat-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
 
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def cat_index(request):
-    cats = Cat.objects.all()
+    cats = Cat.objects.filter(user=request.user)
     return render(request, 'cats/index.html', {'cats': cats})
 
+@login_required
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     # toys = Toy.objects.all()
@@ -26,6 +57,7 @@ def cat_detail(request, cat_id):
         'toys': toys_cat_doesnt_have
         })
 
+@login_required
 def add_feeding(request, cat_id):
     # grab the form data
     form = FeedingForm(request.POST)
@@ -43,45 +75,55 @@ def add_feeding(request, cat_id):
 
 
 # CBVs
-class CatCreate(CreateView):
+class CatCreate(LoginRequiredMixin, CreateView):
     model = Cat
     fields = ['name', 'breed', 'description', 'age']
     success_url = '/cats/'
 
-class CatUpdate(UpdateView):
+    def form_valid(self, form):
+        # attach the user from the request to the user
+        # that will be inside the form data
+        form.instance.user = self.request.user
+        # then we want the form validation that already exists
+        # on the original CreateView to continue to do its own thing
+        return super().form_valid(form)
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
     model = Cat
     # Let's disallow the renaming of a cat by excluding the name field!
     fields = ['breed', 'description', 'age']
 
-class CatDelete(DeleteView):
+class CatDelete(LoginRequiredMixin, DeleteView):
     model = Cat
     success_url = '/cats/'
 
 # TOYS
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
     model = Toy
     fields = '__all__'
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
     model = Toy
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
     model = Toy
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
     model = Toy
     fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url = '/toys/'
 
 
-    # Note that you can pass a toy's id instead of the whole object
+# Note that you can pass a toy's id instead of the whole object
+@login_required
 def associate_toy(request, cat_id, toy_id):
     Cat.objects.get(id=cat_id).toys.add(toy_id)
     return redirect('cat-detail', cat_id=cat_id)
 
+@login_required
 def remove_toy(request, cat_id, toy_id):
     # Look up the cat
     cat = Cat.objects.get(id=cat_id)
